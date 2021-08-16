107c7f9310622d8bbc5776737513e8e382c4ccaa
Anaqi <aafendi@birchhillequity.com>
2021-08-16 10:10:50 -0400

    performed npm install and build

---
+++ streamlit_labelstudio/frontend/build/precache-manifest.dfd269d17f6b4a6cd29662fb58371e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301027e7c8a93b30a96bbc42abf05bf9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "858ab3e8c3406bf03281",
+    "url": "./static/css/2.b789f26a.chunk.css"
+  },
+  {
+    "revision": "858ab3e8c3406bf03281",
+    "url": "./static/js/2.9d62f070.chunk.js"
+  },
+  {
+    "revision": "4ac32ede635c1c8e678e25f80a8b260c",
+    "url": "./static/js/2.9d62f070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68082c05a7ee5a9bc88c",
+    "url": "./static/js/main.5f2abcb9.chunk.js"
+  },
+  {
+    "revision": "2a64b617e6e08c8b68c5",
+    "url": "./static/js/runtime-main.7e58624a.js"
+  }
+]);
